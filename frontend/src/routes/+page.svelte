b54abaf86af78d167c02abb1cb44130ed3fa7cef
t-yng <t.yng.greenmonkey@gmail.com>
2024-10-29 00:25:37 -0700

    フロントエンドのレイアウト実装

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import FileInput from '$lib/components/FileInput.svelte';
+	import { Select, Label, GradientButton, Spinner } from 'flowbite-svelte';
+
+	let selectedLanguage = 'japanese';
+	let summarizing = false;
+	const languages = [
+		{
+			value: 'japanese',
+			name: 'Japanese'
+		},
+		{
+			value: 'english',
+			name: 'English'
+		}
+	];
+
+	const requestSummarize = () => {
+		summarizing = true;
+		// TODO: PDFからサマリを生成するAPIリクエストを送信
+		return new Promise<void>((resolve, reject) => {
+			setTimeout(() => {
+				resolve();
+			}, 2000);
+		});
+	};
+
+	const handleSubmit = async (event: SubmitEvent) => {
+		event.preventDefault();
+		const form = event.target as HTMLFormElement;
+		// TODO: アップロードされたPDFを取得
+		const formData = new FormData(form);
+
+		await requestSummarize();
+		goto('/summarize');
+	};
+</script>
+
+<main class="flex h-full flex-col items-center justify-center">
+	<form
+		class="flex max-w-[800px] flex-col items-center justify-center gap-4"
+		on:submit={handleSubmit}
+	>
+		<!-- Upload paper -->
+		<div class="w-[500px]">
+			<FileInput />
+		</div>
+		<div class="self-start">
+			<Label for="output-language" class="mb-2">Output Language</Label>
+			<Select items={languages} value={selectedLanguage} size="sm" class="w-[150px]" />
+		</div>
+		<GradientButton type="submit" color="purpleToBlue" class="mx-auto block">
+			{#if summarizing}
+				<Spinner class="me-3" size="4" color="white" />Summarizing ...
+			{:else}Summarize{/if}
+		</GradientButton>
+	</form>
+</main>
---
+++ frontend/src/routes/summarize/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Select, A } from 'flowbite-svelte';
+	import { ArrowLeftOutline } from 'flowbite-svelte-icons';
+
+	const sections = [
+		{
+			value: 'abstract',
+			name: 'Abstract'
+		},
+		{
+			value: 'introduction',
+			name: 'Introduction'
+		}
+	];
+</script>
+
+<main class="h-[100%] pb-8 pt-24">
+	<div class="w-max-[1200px] flex h-[100%] w-screen flex-col px-8">
+		<A href="/"><ArrowLeftOutline class="me-2 h-6 w-6" /> Back</A>
+		<div class="mb-4 w-full">
+			<Select items={sections} value="abstract" size="sm" class="w-[150px]" />
+		</div>
+		<div class="grid flex-1 grid-cols-2 gap-8">
+			<div class="border border-solid border-black p-4"></div>
+			<div class="border border-solid border-black p-4"></div>
+		</div>
+	</div>
+</main>
